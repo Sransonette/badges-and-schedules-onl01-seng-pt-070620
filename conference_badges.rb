45601a6c33eaba4a31b94b7e1e3fa9c9aff8e0f5
Sean Ransonette <sransonette@gmail.com>
2020-07-14 01:33:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,4 +12,8 @@ end
 
 def assign_rooms(room_assignments)
   rooms = []
-  
+  room_assignments.each_with_index do |name, index|
+    rooms << ("Hello, #{name}! You'll be assigned to room #{index}!")
+  end
+  rooms
+end
