aec932d758c64b6d3d8401f92b2beb8bd1284ad8
Sean Ransonette <sransonette@gmail.com>
2020-07-14 01:59:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,11 +19,11 @@ def assign_rooms(room_assignments)
 end
 
 def printer(attendees)
-  batch_badge_creator.each do |badge|
+  batch_badge_creator(attendees).each do |badge|
     puts badge
   end
   
-  assign_rooms.each do |room|
+  assign_rooms(attendees).each do |room|
     puts room
   end
 end
