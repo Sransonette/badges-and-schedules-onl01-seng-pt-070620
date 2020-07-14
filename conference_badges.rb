b45725a1b33befa1f1fbed3bf103d5d5fc804073
Sean Ransonette <sransonette@gmail.com>
2020-07-14 01:08:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,6 +5,6 @@ end
 def batch_badge_creator(name)
   attendees = []
   name.each do |badges|
-    attendees << "Hello, my name is #{name}."
+    attendees << ("Hello, my name is #{name}.")
   end
 end
